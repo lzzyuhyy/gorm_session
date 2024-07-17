5c6850366afa4ba5a5800d60cb10bdc2807356aa
zzsuza <17683844709@163.com>
2024-07-17 20:26:55 +0800

    return err 1

--- session/session.go
+++ session/session.go
@@ -4,7 +4,7 @@ import (
 	"gorm.io/gorm"
 )
 
-func SessionAction(db *gorm.DB, actions func(db *gorm.DB) *gorm.DB) {
+func SessionAction(db *gorm.DB, actions func(db *gorm.DB) error) {
 	tx := db.Begin()
 
 	err := actions(tx)
